4830cfe268d2cbc8cf75fe5340a0fc2fff91603d
Konrad1991 <konrad_kraemer@yahoo.de>
2025-08-05 12:14:30 +0200

    removed unnecassary files

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,13 +3,13 @@
 sudo systemctl stop shiny-server
 
 # Stop any running openstats-app container
-var=$(docker container ls | grep 'openstats-app' | awk '{print $1}')
+var=$(docker container ls | grep 'openstats' | awk '{print $1}')
 if [ -n "$var" ]; then
   docker stop "$var"
 fi
 
 # Build the Docker image
-docker build -t openstats-app .
+docker build -t openstats .
 
 # Run the container
-docker run --rm -p 3838:3838 openstats-app
+docker run --rm -p 127.0.0.1:3838:3838 openstats
